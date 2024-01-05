7793da10cd5c0183cded83d595eeabf9e1f054e7
see24 <sarahendicott4@gmail.com>
2024-01-05 12:37:24 -0500

    init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' testCompendium: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Sarah Endicott \email{sarah.endicott@ec.gc.ca}
+#' 
+#' @date 2024/01/05
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+renv::restore()
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
